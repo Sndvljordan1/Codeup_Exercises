3e357612819bb615ebc2b3d8d2ac92deec102df8
Jordan Sandoval <sndvljordan@aol.com>
2015-07-14 11:34:50 -0500

    working on debugging for-loop in php

--- for.php
+++ for.php
@@ -5,7 +5,15 @@ $arg3 = $argv[3];
 if ($arg > $arg2 || $argc < 4){
 	fwrite(STDOUT, "Invalid" . PHP_EOL);
 	fwrite(STDOUT, "Input min, max, and increment after file name." . PHP_EOL);
-	exit(0);
+	fwrite(STDOUT, "Please give minimum " .PHP_EOL);
+	$arg = fgets(STDIN);
+	fwrite(STDOUT, "Please give maximum " .PHP_EOL);
+	$arg2 = fgets(STDIN);
+	fwrite(STDOUT, "Please state increment for iteration " .PHP_EOL);
+	$arg3 = fgets(STDIN);
+	for ($i = $arg; $i <= $arg2; $i += $arg3) {
+    	echo $i . PHP_EOL;
+	};
 }
 if ($argc == 4){
 	for ($i = $arg; $i <= $arg2; $i += $arg3) {
