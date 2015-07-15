7ea427269f5ce2e946bbb8e0d6d3f8429cbaa280
Jordan Sandoval <sndvljordan@aol.com>
2015-07-15 11:31:11 -0500

    finished first function exercise in php using arithmetic functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,23 +1,41 @@
 <?php
-
+$a = 103;
+$b = 22;
 function add($a, $b)
 {
-    echo $a + $b;
+    return $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b)
 {
     // Add code here
+    return $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b)
 {
     // Add code here
+    return $a * $b . PHP_EOL;
 }
 
 function divide($a, $b)
 {
     // Add code here
+    return $a / $b . PHP_EOL;
 }
-
+function modulus($a, $b)
+{
+	return $a % $b . PHP_EOL;
+}
 // Add code to test your functions here
+echo add($a, $b);
+echo " " . PHP_EOL;
+echo subtract($a, $b);
+echo " " . PHP_EOL;
+echo multiply($a, $b);
+echo " " . PHP_EOL;
+echo divide($a, $b);
+echo " " . PHP_EOL;
+echo modulus($a, $b);
+echo " " . PHP_EOL;
+?>
