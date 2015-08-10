325269bbbb7b30776457080858cbc377f313d850
Jordan Sandoval <sndvljordan@aol.com>
2015-08-10 10:23:47 -0500

    updated user.php with update, insert and save methods

--- testmodel.php
+++ testmodel.php
@@ -1,20 +1,8 @@
 <?php
-require_once 'model.php';
+require_once 'user.php';
 
-$flash = new Model;
-$flash->firstName = 'Barry';
-$flash->lastName = 'Allen';
-$flash->alias = 'The Flash';
-$flash->fast = 'super fast';
-$flash->timetravel = 'traversed time multiple times';
-$flash->livingMother = 'mother is dead as a door nail';
+$user = User::all();
+var_dump($user);
 
 
-// echo $flash->firstName . PHP_EOL;
-// echo $flash->lastName . PHP_EOL;
-// echo $flash->alias . PHP_EOL;
-// echo $flash->fast . PHP_EOL;
-// echo $flash->timetravel . PHP_EOL;
-// echo $flash->livingMother . PHP_EOL;
-
-echo USER::getTableName() . PHP_EOL;
+?>
